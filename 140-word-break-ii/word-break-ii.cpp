13eb4efdb681b6ce072aa5d607ca3a0f6f861f72
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-08-16 13:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (95.24%) - LeetSync

---
+++ 140-word-break-ii/word-break-ii.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    unordered_set<string>st;
+    vector<string>result;
+    void solve(int i,string& currSentence, string& s){
+        if(i >= s.length()){
+            result.push_back(currSentence);
+            return;
+        }
+        for(int j=i;j<s.length();j++){
+            string tempWord=s.substr(i,j-i+1);
+            if(st.count(tempWord)){
+                string tempSentence=currSentence;
+                if(!currSentence.empty()){
+                    currSentence+=" ";
+                }
+                currSentence+=tempWord;
+                solve(j+1,currSentence,s);
+                currSentence = tempSentence;
+            }
+        }
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        for(string &word : wordDict){
+            st.insert(word);
+        }
+        string currSentence = "";
+        solve(0,currSentence,s);
+        return result;
+    }
+};
