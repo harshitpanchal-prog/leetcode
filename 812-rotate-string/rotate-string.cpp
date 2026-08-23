f7bd6c7985d13e9b85fc4150bb6ae45237afce49
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-08-23 10:25:05 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (56.51%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -7,11 +7,8 @@ public:
         if(n != m){
             return false;
         }
-        for(int i = 0 ; i < n ; i++){
-            rotate(s.begin(),s.begin()+1,s.end());
-            if(s == goal){
-                return true;
-            }
+        if((s + s).find(goal) != string :: npos){
+            return true;
         }
         return false;
     }
