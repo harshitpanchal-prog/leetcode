ede160e56e0544232d4322c4df4b51c5e760835b
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-08-26 18:37:43 +0530

    Time: 9 ms (33.99%) | Memory: 14.5 MB (13.97%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        for(char& ch:s){
+            if(!st.empty() && st.top() == ch){
+                st.pop();
+            }else{
+                st.push(ch);
+            }
+        }
+        string result="";
+        while(!st.empty()){
+            result+=st.top();
+            st.pop();
+        }
+        reverse(begin(result),end(result));
+        return result;
+    }
+};
