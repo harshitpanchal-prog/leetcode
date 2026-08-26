74f202734623e2f9dd9fb6802a8bb133f05d653a
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-08-26 14:34:56 +0530

    Time: 2 ms (62.38%) | Memory: 11.8 MB (16.52%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    typedef pair<char,int>P;
+    struct lambda{
+        bool operator()(P &p1,P &p2){
+            return p1.second < p2.second;
+        }
+    };
+    string frequencySort(string s) {
+        unordered_map<char,int>mp;
+        priority_queue<P,vector<P>,lambda>pq;
+        
+        for(char& ch:s){
+            mp[ch]++;
+        }
+        for(auto& it : mp){
+            pq.push({it.first,it.second});
+        }
+        string result="";
+        while(!pq.empty()){
+            P temp = pq.top();
+            pq.pop();
+            result+=string(temp.second,temp.first);
+
+        }
+        return result;
+    }
+};
