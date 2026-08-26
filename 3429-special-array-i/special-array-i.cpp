6d3cd65e16d17373f12bdfc57afc8e1b022a3c52
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-08-26 09:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 27.8 MB (28.07%) - LeetSync

--- 3429-special-array-i/special-array-i.cpp
+++ 3429-special-array-i/special-array-i.cpp
@@ -3,7 +3,7 @@ public:
     bool isArraySpecial(vector<int>& nums) {
         int n=nums.size();
         for(int i=0 ; i<=n-2 ; i++){
-            if((nums[i]% 2) == (nums[i+1] %2)){
+            if((nums[i] & 1) == (nums[i+1] & 1)){
                 return false;
             }
         }
