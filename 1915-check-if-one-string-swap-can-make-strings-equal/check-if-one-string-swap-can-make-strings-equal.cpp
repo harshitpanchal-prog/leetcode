c6b33dce8006469b1b714ac9428267bc21448a5a
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-08-29 11:24:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (87.38%) - LeetSync

--- 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1915-check-if-one-string-swap-can-make-strings-equal/check-if-one-string-swap-can-make-strings-equal.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
         int n=s1.length();
-        if(s1 == s2){
-            return true;
-        }
-        int diffs = 0;
-        int freq1[26] = {0};
-        int freq2[26] = {0};
+        int diffs=0;
+        int first_diff=0;
+        int second_diff=0;
         for(int i=0 ; i<n ; i++){
             if(s1[i] != s2[i]){
                 diffs++;
-            }
-            if(diffs >2){
-                return false;
-            }
-            freq1[s1[i]-'a']++;
-            freq2[s2[i]-'a']++;
-        }
-        for(int i=0 ; i<26 ;i++){
-            if(freq1[i]!=freq2[i]){
-                return false;
+                if(diffs > 2){
+                    return false;
+                }else if(diffs == 1){
+                    first_diff=i;
+                }else{
+                    second_diff=i;
+                }
             }
         }
-        return true;
+        return s1[first_diff] == s2[second_diff] && s1[second_diff] == s2[first_diff];
     }
 };
