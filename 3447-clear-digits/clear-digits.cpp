a0c362c6e1703f6779778145d5d87222447db76c
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-08-29 13:37:02 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (5.21%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        int i=0;
-        while(i<s.length()){
-            if(isdigit(s[i])){
-                s.erase(i,1);
-                if(i-1>=0){
-                    s.erase(i-1,1);
-                    i--;
+        stack<int> st;
+        for(char& ch:s){
+            if(isdigit(ch)){
+                if(!st.empty()){
+                    st.pop();
                 }
             }else{
-                i++;
+                st.push(ch);
             }
         }
-        return s;
+        string result="";
+        while(!st.empty()){
+            result.push_back(st.top());
+            st.pop();
+        }
+        reverse(begin(result),end(result));
+        return result;
     }
 };
