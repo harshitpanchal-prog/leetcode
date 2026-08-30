3bf6c6c86c00a54fa718dc1e3b76ff78f42dbad8
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-08-30 08:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (91.53%) - LeetSync

--- 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ 2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
         int n=nums.size();
-        for(int i=0 ; i< n-1 ;i++){
-            if(nums[i] == nums[i+1]){
+        int j=0;
+        for(int i=0 ; i<n ; i++){
+            if(i+1 < n && nums[i]==nums[i+1] && nums[i]!=0){
                 nums[i]*=2;
                 nums[i+1]=0;
             }
-        }
-        int j=0;
-        for(int i=0 ; i <n ;i++){
-            if(nums[i] != 0){
-                nums[j]=nums[i];
+            if(nums[i]!=0){
+                if(i!=j){
+                    swap(nums[i],nums[j]);
+                }
                 j++;
             }
-        }
-        while(j<n){
-            nums[j]=0;
-            j++;
         }
         return nums;
     }
