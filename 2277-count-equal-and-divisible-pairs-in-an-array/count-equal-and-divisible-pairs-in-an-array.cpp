55cfe2752dcec2b232f3562ae48db8c7e2ab1535
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-09-06 11:55:40 +0530

    Time: 2 ms (39.98%) | Memory: 16.9 MB (5.43%) - LeetSync

--- 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/count-equal-and-divisible-pairs-in-an-array.cpp
@@ -2,14 +2,33 @@ class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
         int n=nums.size();
-        int result=0;
-        for(int i=0 ; i<n-1 ; ++i){
-            for(int j=i+1 ; j<n ; ++j){
-                if((i*j)%k == 0 && nums[i] == nums[j]){
-                    result++;
+        long long result=0;
+        unordered_map<int ,vector<int>>indices_map;
+        for(int i=0 ; i<n ; i++){
+            indices_map[nums[i]].push_back(i);
+        }
+
+        unordered_set<int>div;
+        for(int f=1 ; f*f<=k ; f++){
+            if(k%f == 0 ){
+                div.insert(f);
+                div.insert(k/f);
+            }
+        }
+        for(auto&[num,indices] : indices_map){
+            unordered_map<int,int>factors_map;
+            for(int i : indices){
+                int GCD = gcd(i,k);
+                int j = k/GCD;
+                result+=factors_map[j];
+                for(int f : div){
+                    if(i%f ==0 ){
+                        factors_map[f]++;
+                    }
                 }
             }
         }
         return result;
+
     }
 };
