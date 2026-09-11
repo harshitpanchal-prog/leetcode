2c872bb1ac69ce37b59ca086dda7ed65d27fc06d
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-09-11 14:20:59 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (78.51%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
-    int countones(int num){
-        int count=0;
-        while(num){
-            count+=(num&1);
-            num>>=1;
-        }
-        return count;
-    }
-
+    
     vector<int> sortByBits(vector<int>& arr) {
-        auto lambda = [&](int &a , int &b){
-            int count_a=countones(a);
-            int count_b=countones(b);
+        auto lambda = [](int &a , int &b){
+            int count_a=__builtin_popcount(a);
+            int count_b=__builtin_popcount(b);
             if(count_a == count_b){
                 return a<b;
             }
