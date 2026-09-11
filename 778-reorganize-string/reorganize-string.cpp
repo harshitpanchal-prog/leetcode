7fcacbd7c7d4bbaf91b04c5be0df7423d6effadb
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-09-11 16:24:59 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (97.73%) - LeetSync

--- 778-reorganize-string/reorganize-string.cpp
+++ 778-reorganize-string/reorganize-string.cpp
@@ -1,41 +1,38 @@
 class Solution {
 public:
-    typedef pair<int,char>P;
+    
     string reorganizeString(string s) {
         int n=s.length();
         vector<int> count(26,0);
+        int maxfreq = 0;
+        int charmaxfreq;
         for(char& ch : s){
             count[ch-'a']++;
+            if(count[ch-'a']>maxfreq){
+                maxfreq = count[ch-'a'];
+                charmaxfreq = ch;
+            }
             if(count[ch-'a']>(n+1)/2){
                 return "";
             }
         }
 
-        priority_queue<P,vector<P>>pq;
+        string result=s;
+        int i=0;
+        while(count[charmaxfreq-'a']>0){
+            result[i] = charmaxfreq;
+            i+=2;
+            count[charmaxfreq-'a']--;
+        }
         for(char ch='a' ; ch<='z' ; ch++){
-            if(count[ch-'a']>0){
-                pq.push({count[ch-'a'],ch});
+            while(count[ch-'a']>0){
+                if(i>=n){
+                    i=1;
+                }
+                result[i]=ch;
+                i+=2;
+                count[ch-'a']--;
             }
-        }
-        string result="";
-        while(pq.size() >= 2){
-            auto P1 = pq.top();
-            pq.pop();
-            auto P2 = pq.top();
-            pq.pop();
-            result.push_back(P1.second);
-            P1.first--;
-            result.push_back(P2.second);
-            P2.first--;
-            if(P1.first > 0){
-                pq.push(P1);
-            }
-            if(P2.first > 0){
-                pq.push(P2);
-            }
-        }
-        if(!pq.empty()){
-            result.push_back(pq.top().second);
         }
         return result;
     }
