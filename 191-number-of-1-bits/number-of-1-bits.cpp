9203cf50a1b07c5ea681564078491c621bff1b27
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-09-22 07:52:32 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (81.78%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -3,8 +3,8 @@ public:
     int hammingWeight(int n) {
         int count = 0;
         while(n>0){
-            n=(n&(n-1));
-            count++;
+            count+=(n%2);
+            n/=2;
         }
         return count;
     }
