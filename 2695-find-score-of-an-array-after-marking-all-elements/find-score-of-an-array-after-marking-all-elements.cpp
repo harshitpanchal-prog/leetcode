60d472707dc3d353d48a0fe6994047eabfc91649
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-09-25 09:51:19 +0530

    Time: 127 ms (39.05%) | Memory: 105.8 MB (48.90%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+    #define P pair<int,int>
     long long findScore(vector<int>& nums) {
         int n = nums.size();
-        vector <pair<int,int>> vec(n);
-        for(int i=0 ; i<n ; i++){
-            vec[i]={nums[i],i};
-        }
         vector<bool>visited(n,false);
-        long long score = 0;
-        sort(begin(vec) , end(vec));
+        priority_queue<P,vector<P>,greater<P>>pq;
         for(int i=0 ; i<n ; i++){
-            int element = vec[i].first;
-            int idx = vec[i].second;
-            if(visited[idx] == false){
-                visited[idx]==true;
+            pq.push({nums[i],i});
+        }
+        long long score=0;
+        while(!pq.empty()){
+            auto temp = pq.top();
+            int element = temp.first;
+            int idx = temp.second;
+            pq.pop();
+            if(!visited[idx]){
+                visited[idx]=true;
                 score+=element;
-                if(idx-1>=0 && visited[idx-1] == false){
+                if(idx-1 >= 0 && !visited[idx-1]){
                     visited[idx-1]=true;
                 }
-                if(idx+1<n && visited[idx+1] == false){
+                if(idx+1 < n && !visited[idx+1]){
                     visited[idx+1]=true;
                 }
-
             }
         }
         return score;
