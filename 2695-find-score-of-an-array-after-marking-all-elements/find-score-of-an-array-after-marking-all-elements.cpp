8601d00b62a424aa1c30b45a2d656fbfa23e230e
harshitpanchal-prog <harshit.ajpanchal@gmail.com>
2026-09-25 09:26:02 +0530

    Time: 61 ms (87.78%) | Memory: 98.9 MB (89.13%) - LeetSync

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    long long findScore(vector<int>& nums) {
+        int n = nums.size();
+        vector <pair<int,int>> vec(n);
+        for(int i=0 ; i<n ; i++){
+            vec[i]={nums[i],i};
+        }
+        vector<bool>visited(n,false);
+        long long score = 0;
+        sort(begin(vec) , end(vec));
+        for(int i=0 ; i<n ; i++){
+            int element = vec[i].first;
+            int idx = vec[i].second;
+            if(visited[idx] == false){
+                visited[idx]==true;
+                score+=element;
+                if(idx-1>=0 && visited[idx-1] == false){
+                    visited[idx-1]=true;
+                }
+                if(idx+1<n && visited[idx+1] == false){
+                    visited[idx+1]=true;
+                }
+
+            }
+        }
+        return score;
+    }
+};
